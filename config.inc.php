3e5beafff805e84408fca3064fb1aea517cb0a9a
patrickcs1 <33843688+patrickcs1@users.noreply.github.com>
2017-12-05 15:11:27 -0200

    Update config.inc.php
    Configuration username Database

--- config.inc.php
+++ config.inc.php
@@ -29,7 +29,6 @@ $i++;
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
 $cfg['Servers'][$i]['host'] = 'mysql';
-$cfg['Servers'][$i]['user'] = 'root';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = true;
 
