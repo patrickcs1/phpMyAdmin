f8daa0d7a6bc18e0dfd398d46d561b4880c02448
patrickcs1 <33843688+patrickcs1@users.noreply.github.com>
2017-12-05 14:40:30 -0200

    Update.inc.php

--- config.inc.php
+++ config.inc.php
@@ -29,6 +29,7 @@ $i++;
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
 $cfg['Servers'][$i]['host'] = 'mysql';
+$cfg['Servers'][$i]['user'] = 'root';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = true;
 
