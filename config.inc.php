96e99ed552ba1e2aaf94e01a8f38ace1ac73e280
patrickcs1 <33843688+patrickcs1@users.noreply.github.com>
2017-12-05 15:17:51 -0200

    Update config.linc.php
    Configuration username Database

--- config.inc.php
+++ config.inc.php
@@ -31,6 +31,8 @@ $cfg['Servers'][$i]['auth_type'] = 'cookie';
 $cfg['Servers'][$i]['host'] = 'mysql';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = true;
+$cfg['Servers'][$i]['user'];
+
 
 /**
  * phpMyAdmin configuration storage settings.
